7578810b7e07f1b2c4f7e8473a771086c0efed4b
hexaeight <78523248+hexaeight@users.noreply.github.com>
2022-05-12 23:01:15 +0530

    Update captcha-min.js

--- captcha.js/v1.6.8-03/captcha-min.js
+++ captcha.js/v1.6.8-03/captcha-min.js
@@ -46,7 +46,7 @@ function widgetQRCode(_this,_fetchCaptchaUrl,_captchaValidationUrl) {
                 captchaUrl1 = split[0];
                 captchaUrl2 = split[3]+'|'+split[4];
                 $('.loader').hide();
-                var modalpopup = "<div id='myModal' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header align-center'><h4 class='modal-title'>Use <a href='https://www.hexaeight.com/help/how-to-create-your-first-digital-token.html' target='_blank'>HexaEight Digital Token</a> To Scan This QR Code</h4></div><div class='modal-body'><div class='loader'></div><div id='qrcode'></div></div></div></div></div>"; 
+                var modalpopup = "<div id='myModal' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header div'><h4 class='modal-title'>Use <a href='https://www.hexaeight.com/help/how-to-create-your-first-digital-token.html' target='_blank'>HexaEight Digital Token</a> To Scan This QR Code</h4></div><div class='modal-body'><div class='loader'></div><div id='qrcode'></div></div></div></div></div>"; 
                 $('body').append(modalpopup);   
                 $('#myModal').modal({backdrop: 'static', keyboard: false});
                 $("#myModal").modal('show');
